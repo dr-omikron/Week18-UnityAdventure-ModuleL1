b188e7dc299a75aef0d394763f1722bb30f73815
dr-omikron <76596650+dr-omikron@users.noreply.github.com>
2026-01-18 12:17:27 +0400

    Update DIContainer.cs

--- Assets/_Project/Develop/Runtime/Infrastructure/DI/DIContainer.cs
+++ Assets/_Project/Develop/Runtime/Infrastructure/DI/DIContainer.cs
@@ -6,6 +6,7 @@ namespace _Project.Develop.Runtime.Infrastructure.DI
     public class DIContainer
     {
         private readonly Dictionary<Type, Registration> _container = new Dictionary<Type, Registration>();
+        private readonly List<Type> _requests = new List<Type>();
 
         public void RegisterAsSingle<T>(Func<DIContainer, T> creator)
         {
@@ -15,8 +16,20 @@ namespace _Project.Develop.Runtime.Infrastructure.DI
 
         public T Resolve<T>()
         {
-            if (_container.TryGetValue(typeof(T), out Registration registration))
-                return (T)registration.CreateInstanceFrom(this);
+            if(_requests.Contains(typeof(T)))
+                throw new InvalidOperationException($"Cannot resolve for {typeof(T)}");
+
+            _requests.Add(typeof(T));
+
+            try
+            {
+                if (_container.TryGetValue(typeof(T), out Registration registration))
+                    return (T)registration.CreateInstanceFrom(this);
+            }
+            finally
+            {
+                _requests.Remove(typeof(T));
+            }
 
             throw new InvalidOperationException($"Could not resolve type {typeof(T)}");
         }
