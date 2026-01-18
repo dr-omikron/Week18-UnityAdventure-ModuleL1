3125b0bb1e57fe613000d696669600cebd18411b
dr-omikron <76596650+dr-omikron@users.noreply.github.com>
2026-01-18 11:57:30 +0400

    created DIContainer

---
+++ Assets/_Project/Develop/Runtime/Infrastructure/DI/DIContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _Project.Develop.Runtime.Infrastructure.DI
+{
+    public class DIContainer
+    {
+        private readonly Dictionary<Type, Registration> _container = new Dictionary<Type, Registration>();
+
+        public void RegisterAsSingle<T>(Func<DIContainer, T> creator)
+        {
+            Registration registration = new Registration(container => creator.Invoke(container));
+            _container.Add(typeof(T), registration);
+        }
+
+        public T Resolve<T>()
+        {
+            if (_container.TryGetValue(typeof(T), out Registration registration))
+                return (T)registration.CreateInstanceFrom(this);
+
+            throw new InvalidOperationException($"Could not resolve type {typeof(T)}");
+        }
+    }
+}
